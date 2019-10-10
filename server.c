d351571ffedd1eba3260306a68688157ee727d09
SungyongHong <45868367+SungyongHong@users.noreply.github.com>
2019-10-10 21:36:25 +0900

    Update server.c

--- server.c
+++ server.c
@@ -4,103 +4,112 @@
 #include <string.h>
 
 #define PORT 10000
-char * delsp(char * dest, char * cmd);
-int cmp(char *str);
-char buffer[100] = "Hi, I'm server.\n";
-char rcvBuffer[100];
+
+char snBf[100];
+char rcvBf[100];
+
 int main(){
 	int c_socket, s_socket;
 	struct sockaddr_in s_addr, c_addr;
 	int len;
 	int n;
+	int chlen; //strlen -- 처리용
+	char chlen_str[100]; //chlen을 write 하는 도중 sprintf을 위한 변수 
+	int chcmp; //	strcmp -- 처리용
+	int result;
+	char cmp_str[100];	//cmp결과 담을 변수
 
-	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+	// 1. 서버 소켓 생성
+	//서버 소켓 = 클라이언트의 접속 요청을 처리(허용)해 주기 위한 소켓
+	s_socket = socket(PF_INET, SOCK_STREAM, 0); //TCP/IP 통신을 위한 서버 소켓 생성
 	
-	memset(&s_addr, 0, sizeof(s_addr)); 
-	s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	//2. 서버 소켓 주소 설정
+	memset(&s_addr, 0, sizeof(s_addr)); //s_addr의 값을 모두 0으로  초기화
+	s_addr.sin_addr.s_addr = htonl(INADDR_ANY); //IP 주소 설정
 	s_addr.sin_family = AF_INET;
 	s_addr.sin_port = htons(PORT);
 
-
+	//3. 서버 소켓바인딩
 	if(bind(s_socket,(struct sockaddr *) &s_addr, sizeof(s_addr)) == -1){ 
+		//바인딩 작업 실패 시, Cannot Bind 메시지 출력 후 프로그램 종료
 		printf("Cannot Bind\n");
 		return -1;
 	}
 	
+	//4.listen() 함수 실행
 	if(listen(s_socket, 5) == -1){
 		printf("listen Fail\n");
 		return -1;
 	}
-	while(1){
+
+	//5. 클라이언트 요청 처리
+	// 요청을 허용한 후, Hello World 메세지를 전송함
+	while(1){ //무한 루프
 		len = sizeof(c_addr);
-		printf("Wait connect client\n");
+		printf("클라이언트 접속을 기다리는 중....\n");
 		c_socket = accept(s_socket, (struct sockaddr *)&c_addr, &len); 
+		//클라이언트의 요청이 오면 허용(accept)해 주고, 해당 클라이언트와 통신할 수 있도록 클라이언트 소켓(c_socket)을 반환함.
 		printf("/client is connected\n");
-		while(1){
-			n=read(c_socket, rcvBuffer, sizeof(rcvBuffer));
-			printf("rcvBuffer: %s\n", rcvBuffer);
+		printf("클라이언트 접속 허용\n");
 
-			if(!strncmp(rcvBuffer, "Hi", 2)){
-				char rcvBf[100] = "Hi, nice meet you!";
-				write(c_socket, rcvBf, strlen(rcvBf));	
-			}
+		while(1){	
 
-			else if(!strncmp(rcvBuffer, "What is your name?", 18)){
-				char rcvBf[100] = "My name is Hongsungyong!";
-				write(c_socket, rcvBf, strlen(rcvBf));
-			}
+			n=read(c_socket, rcvBf, sizeof(rcvBf));
+			rcvBf[n]='\0'; //개행문자포함시켜서 오류막기
+			printf("Received Data: %s\n",rcvBf);  // 체크용도
 
-			else if(!strncmp(rcvBuffer, "How old are you?", 16)){
-				char rcvBf[100] = "I am 23 years old!";
-				write(c_socket, rcvBf, strlen(rcvBf));
-			}
-
-			else if(!strncmp(rcvBuffer, "quit", 4)|| !strncmp(rcvBuffer, "kill server", 11)){
+			if(strncasecmp(rcvBf,"quit",4)==0 || strncasecmp(rcvBf,"kill server",11)==0)
 				break;
-			}
 
-			else if(!strncmp(rcvBuffer,"strlen", 6)){
-				char * bf = delsp(rcvBuffer, "strlen");
-				write(c_socket, bf, strlen(bf)-1);
+			if(strncasecmp(rcvBf,"안녕하세요\n",n)==0||strncasecmp(rcvBf,"안녕하세요.\n",n)==0){
+				strcpy(snBf,"안녕하세요. 만나서 반가워요!\n");
+				write(c_socket,snBf,strlen(snBf));
 			}
-			else if(!strncmp(rcvBuffer,"strcmp", 6)){
-				char * bf = delsp(rcvBuffer, "strcmp");
-				int last = cmp(bf);
-				sprintf(bf, " %d\n", last);
-				write(c_socket, bf, strlen(bf));		
-			}else{
-			 write(c_socket, "Error", strlen(rcvBuffer)); 
+			else if(strncasecmp(rcvBf,"이름이 머야?\n",n)==0||strncasecmp(rcvBf,"이름이 머야?\n",n)==0){
+				strcpy(snBf,"내 이름은 홍성용이야.\n");
+				write(c_socket,snBf,strlen(snBf));			
 			}
+			else if(strncasecmp(rcvBf,"몇 살이야?\n",n)==0||strncasecmp(rcvBf,"몇 살이야?\n",n)==0){
+				strcpy(snBf,"나는 23살이야.\n");
+				write(c_socket,snBf,strlen(snBf));
+			}
+			else if(strncasecmp(rcvBf,"strlen",6)==0){
+				chlen=strlen(rcvBf)-8;
+				sprintf(chlen_str,"문자열의 길이= %d\n",chlen);
+				strcpy(snBf,chlen_str);
+				write(c_socket,snBf,strlen(snBf));
+			}
+			else if(strncasecmp(rcvBf,"strcmp",6)==0){
+				char *ptr=strtok(rcvBf," ");
+				ptr=strtok(NULL," ");
+				char *str1=ptr;
+				ptr=strtok(NULL," ");
+				char *str2=ptr;
+				str2[strlen(str2)-1]='\0';
+
+				printf("%s %d ",str1,strlen(str1));
+				printf("%s %d\n",str2,strlen(str2));
+
+				if (strcmp(str1,str2)==0){
+					result=0;
+					sprintf(cmp_str,"비교결과= %d ---> [일치]\n",result);
+					strcpy(snBf,cmp_str);
+					write(c_socket,snBf,strlen(snBf));
+				}
+				else if(strcmp(str1,str2)!=0){
+					result=strcmp(str1,str2);
+					sprintf(cmp_str,"비교결과= %d ---> [불일치]\n",result);
+					strcpy(snBf,cmp_str);
+					write(c_socket,snBf,strlen(snBf));
+				}
+			}
+			else
+				write(c_socket, rcvBf, n); //모든 조건을 만족하지 않는경우 => 에코
 		}
-
-		
 		close(c_socket);
-		if (strncasecmp(rcvBuffer, "kill server", 11) == 0)
+		if(strncasecmp(rcvBf,"kill server",11)==0)
 			break;
 	}
 	close(s_socket);
 	return 0;	
-}
-
-char * delsp(char * dest, char * cmd){
-	int n = 0;
-	while( * dest == ' ' || *cmd == ' '){
-		n++;
-		dest++;
-		cmd++;
-	}
-	while(*dest!='\0' && *cmd != '\0' && *dest == *cmd){
-		n++;
-		dest++;
-		cmd++;
-	}
-	return dest;
-}
-
-int cmp(char *str){
-	int result;
-	char * t1 = strtok(str," ");
-	str = strtok(NULL, "\n");
-	result = strcmp(t1, str);
-	return result;
 }
